aeee678b8285aeb22ee96f4594f601783a393edf
suannelson123 <nelsonsuan12@gmail.com>
2026-02-24 02:21:21 -0800

    alr

--- loader.lua
+++ loader.lua
@@ -1,6 +1,2 @@
-pcall(coroutine.yield, true, 1, game)
-pcall(coroutine.yield, true, 2, workspace)
-pcall(coroutine.yield, true, 3, script)
-
 local scriptUrl = "https://raw.githubusercontent.com/buhayhayahay332-lang/Compx/main/Operation%20One/main.lua"
 loadstring(game:HttpGet(scriptUrl, true))()
